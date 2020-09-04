e1ca8638440b4ed280c3b8cf37aee48ba3729df1
Coyocon <Leroz_@hotmail.com>
2020-09-04 12:16:24 -0500

    Menu-slider

---
+++ Menu-slider/main.js
@@ -0,0 +1,11 @@
+document.querySelector('.desple').addEventListener('click',()=>{
+    document.querySelector('.contac').classList.toggle('show');
+});
+
+document.querySelector('.menu-btn').addEventListener('click',()=>{
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+$(document).ready(function(){
+    $('.slider').bxSlider();
+  });
